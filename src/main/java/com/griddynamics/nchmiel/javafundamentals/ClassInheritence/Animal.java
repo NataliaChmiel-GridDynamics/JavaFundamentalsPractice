1109316784868592605ff809de1a4d301bef9084
Natalia Chmiel <nchmiel@griddynamics.com>
2024-03-20 16:45:00 +0100

    packages names edited and one exercise file  added

---
+++ src/main/java/com/griddynamics/nchmiel/javafundamentals/ClassInheritence/Animal.java
@@ -0,0 +1,16 @@
+package com.griddynamics.nchmiel.javafundamentals.ClassInheritence;
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+class Bird extends Animal{
+    void fly(){
+        System.out.println("I am flying");
+    }
+    void sing(){
+        System.out.println("I am singing!");
+    }
+}
+
---
+++ src/main/java/com/griddynamics/nchmiel/javafundamentals/ClassInheritence/Solution.java
@@ -0,0 +1,13 @@
+package com.griddynamics.nchmiel.javafundamentals.ClassInheritence;
+
+public class Solution{
+
+    public static void main(String args[]){
+
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+
+    }
+}
